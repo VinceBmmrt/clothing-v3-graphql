a6bfaf7de69c87cced71140547ada08232859308
VinceBmmrt <vincebommert75@gmail.com>
2024-07-25 17:50:50 +0200

    Updating Components To Consume GraphQL Values

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  
+    "tabWidth": 2,          // Nombre d'espaces pour une indentation
+    "useTabs": false,       // Utiliser des espaces au lieu de tabulations
+    "singleQuote": true,    // Utiliser des guillemets simples au lieu de doubles
+    "trailingComma": "es5", // Ajouter une virgule après le dernier élément dans les objets, tableaux, etc.
+    "semi": true,           // Ajouter un point-virgule à la fin des lignes
+    "printWidth": 80,       // Largeur maximale de ligne avant le passage à la ligne suivante
+    "bracketSpacing": true, // Ajouter des espaces entre les crochets
+    "jsxBracketSameLine": false // Positionner le `>` de fermeture des balises JSX sur la même ligne que le dernier attribut
+  
+  };
+  
--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import App from './App';
-import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
+import { CategoriesProvider } from './contexts/categories.context';
+import { UserProvider } from './contexts/user.context';
 
 import './index.scss';
-
+const client = new ApolloClient({
+  uri:"https://crwn-clothing.com/",
+  cache: new InMemoryCache()
+})
 const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
+  <ApolloProvider client={client}>
     <BrowserRouter>
       <UserProvider>
         <CategoriesProvider>
@@ -22,6 +27,7 @@ render(
         </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
   rootElement
 );
